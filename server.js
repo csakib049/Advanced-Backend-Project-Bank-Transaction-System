7a8e0c636d935e4153764b7957607fd5d95a82dd
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-06-01 11:25:44 +0600

    connecting with db

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const app = require("./src/app");
+const connectDB = require("./src/config/db")
 
+connectDB();
 
-const PORT = process.evn.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+
+    console.log(`server is running on port ${PORT}.`);
     
 })
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require ("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 
 const app = express()
-
-
 
 
 module.exports =app;
